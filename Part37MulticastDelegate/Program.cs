ef15227e6fb677fbe93b7f38d3b0e7f2197d7e0c
priyasonar1207 <83229578+priyasonar1207@users.noreply.github.com>
2021-10-07 15:41:47 +0530

    Add project files.

---
+++ Part37MulticastDelegate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Part37MulticastDelegate
+{
+    class Program
+    {
+        public delegate void MultiDelegate();
+
+        static void Main(string[] args)
+        {
+            MultiDelegate mlt = new MultiDelegate(Hello1);
+            mlt += (Hello2);
+            mlt += (Hello3);
+            mlt();
+            
+        }
+        public static void Hello1()
+        {
+            Console.WriteLine("Hello1");
+        }
+        public static void Hello2()
+        {
+            Console.WriteLine("Hello2");
+        }
+        public static void Hello3()
+        {
+            Console.WriteLine("Hello3");
+        }
+    }
+}
